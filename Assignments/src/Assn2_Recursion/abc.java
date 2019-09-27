0f020f7bdb80f54977b404af548e0f2d430a46d0
Bryson <bryson.meiling@aggiemail.usu.edu>
2019-09-27 11:38:04 -0600

    first commit placing everything into packages in same project

---
+++ Assignments/src/Assn2_Recursion/abc.java
@@ -0,0 +1,17 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class abc {
+    static String a;
+    public static void main(String[] args) {
+        float num = 0x123_456;
+
+        short s1 = 42, s2 = 24, sum = 0;
+        sum += s1;
+        sum += s2;
+
+        System.out.println(abc.a);
+        System.out.println(a);
+        System.out.println(new Exception());
+        int array[] = {1, 2, 3};
+    }
+}
