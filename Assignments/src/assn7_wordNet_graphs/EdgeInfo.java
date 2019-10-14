12fd6c9b58d815bb134288318a62deda819f35f2
Bryson <bryson.meiling@aggiemail.usu.edu>
2019-10-14 10:31:43 -0600

    assn 5-7 added

---
+++ Assignments/src/assn7_wordNet_graphs/EdgeInfo.java
@@ -0,0 +1,18 @@
+package assn7_wordNet_graphs;
+
+public class EdgeInfo {
+
+    public EdgeInfo(int from, int to){
+        this.from = from;
+        this.to = to;
+
+    }
+    public String toString(){
+        return from + "->" + to + " " ;
+    }
+
+    int from;        // source of edge
+    int to;          // destination of edge
+
+
+}
